43864923fccee16f188dc9f2365c87fb0802d3dd
angcyo <angcyo@126.com>
2015-12-18 15:27:38 +0800

    2015-12-18 大转盘view

--- HKCloud/src/com/huika/cloud/control/me/view/RsenTestActivity.java
+++ HKCloud/src/com/huika/cloud/control/me/view/RsenTestActivity.java
@@ -4,6 +4,7 @@ import android.view.ViewGroup;
 
 import com.huika.cloud.R;
 import com.zhoukl.androidRDP.RdpFramework.RdpActivity.RdpBaseActivity;
+import com.zhoukl.androidRDP.RdpUtils.RdpAnnotationUtil;
 
 /**
  * Created by angcyo on 15-12-18 018 15:16 下午.
@@ -14,6 +15,8 @@ public class RsenTestActivity extends RdpBaseActivity {
     @Override
     protected void initActivity() {
         super.initActivity();
+        setFuncTitle("大转盘");
         mRootLayout = (ViewGroup) addMasterView(R.layout.rsen_test_layout);
+        RdpAnnotationUtil.inject(this);
     }
 }
